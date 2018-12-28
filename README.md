50b1874985f5275239c5e82b011d23a8459a6d54
jjwmezun <jjwmezun@gmail.com>
2018-12-27 17:58:55 -0800

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+WP Theme Picture
+=========================
+
+Class for autogenerating picture tag HTML for WordPress theme image.
---
+++ src/WPThemePicture.php
@@ -0,0 +1,20 @@
+<?php
+
+declare( strict_types = 1 );
+namespace WaughJ\WPThemePicture
+{
+	use WaughJ\HTMLPicture\HTMLPicture;
+	use WaughJ\WPThemeImage\WPThemeImage;
+
+	class WPThemePicture extends HTMLPicture
+	{
+		public function __construct( string $src, string $extension, $sizes, array $attributes = [] )
+		{
+			WPThemeImage::setDefaultSharedDirectory( 'img' );
+			$loader = WPThemeImage::getFileLoader( $attributes );
+			unset( $attributes[ 'directory' ] ); // Make sure we don't keep this is an attribute that gets passed into the HTML itself.
+			$attributes[ 'loader' ] = $loader;
+			parent::__construct( $src, $extension, $sizes, $attributes );
+		}
+	}
+}
