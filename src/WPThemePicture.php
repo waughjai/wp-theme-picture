d69341c17851ccdc858a374360502cc0c880e799
Jaimeson Waugh <jaimeson@4cesi.com>
2019-04-04 16:16:56 -0700

    Fix bug wherein constructor changes WPThemeImage default dir

--- src/WPThemePicture.php
+++ src/WPThemePicture.php
@@ -10,9 +10,8 @@ namespace WaughJ\WPThemePicture
 	{
 		public function __construct( string $src, string $extension, $sizes, array $attributes = [] )
 		{
-			WPThemeImage::setDefaultSharedDirectory( 'img' );
 			$loader = WPThemeImage::getFileLoader( $attributes );
-			unset( $attributes[ 'directory' ] ); // Make sure we don't keep this is an attribute that gets passed into the HTML itself.
+			unset( $attributes[ 'directory' ] ); // Make sure we don't keep this as an attribute that gets passed into the HTML itself.
 			$attributes[ 'loader' ] = $loader;
 			$picture = self::generate( $src, $extension, $sizes, $attributes );
 			parent::__construct( $picture->getFallbackImage(), $picture->getSources(), $picture->getPictureAttributes()->getAttributes() );
